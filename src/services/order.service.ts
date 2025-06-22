c6fde864d723e189d34e82ef576c881c3083c6fd
Randy <rand4035@gmail.com>
2025-06-22 16:53:46 +0700

    feat: hanldeUpdatePayment

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -3,9 +3,9 @@ import endpoint from "./endpoint.constant";
 import { ICart } from "@/types/Ticket";
 
 const orderServices = {
-  createOrder: (payload: ICart) =>
-    instance.post(endpoint.ORDER, payload),
-
+  createOrder: (payload: ICart) => instance.post(endpoint.ORDER, payload),
+  updateOrderStatus: (id: string, status: string) =>
+    instance.put(`${endpoint.ORDER}/${id}/${status}`),
 };
 
 export default orderServices;
